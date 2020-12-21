7f882817648b0f2871ff7198088b5d1cd2593bea
Dennis <denzel@kds-online.com>
2020-12-21 19:23:55 +0100

    Create advanced_specials.php

---
+++ admin/includes/languages/english/advanced_specials.php
@@ -0,0 +1,69 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+const TEXT_ADVANCED_SPECIALS_VERSION = 'V2.0';
+
+const HEADING_TITLE = 'Advanced Specials';
+
+const TEXT_ADVANCED_SPECIALS_FILTER = 'Filter';
+const TEXT_ADVANCED_SPECIALS_NAME = 'Productname';
+const TEXT_ADVANCED_SPECIALS_CATEGORIE = 'All Categories';
+const TEXT_ADVANCED_SPECIALS_MANUFACTURER = 'All Manufacturers';
+
+const TEXT_ADVANCED_SPECIALS_SORT = 'Sort by:';
+const TEXT_ADVANCED_SPECIALS_ASC = 'Ascending';
+const TEXT_ADVANCED_SPECIALS_DESC = 'Descending';
+const TEXT_ADVANCED_SPECIALS_INCLUDE_SUBCATEGORIES = 'include subcategories';
+const TEXT_ADVANCED_SPECIALS_ONLY_SPECIALS = 'only specials';
+const TEXT_ADVANCED_SPECIALS_ONLY_EXPIRED_SPECIALS = 'expired';
+const TEXT_ADVANCED_SPECIALS_ONLY_ACTIV_PRODUCTS = 'only active products';
+const TEXT_ADVANCED_SPECIALS_SORT_PID = 'Products ID';
+const TEXT_ADVANCED_SPECIALS_SORT_PMODEL = 'Products Model';
+const TEXT_ADVANCED_SPECIALS_SORT_PPRICE = 'Products Price';
+const TEXT_ADVANCED_SPECIALS_SORT_PSTATUS = 'Products Status';
+const TEXT_ADVANCED_SPECIALS_SORT_PNAME = 'Products Name';
+const TEXT_ADVANCED_SPECIALS_APPLY_FILTER = 'Apply filter';
+const TEXT_ADVANCED_SPECIALS_REMOVE_FILTER = 'Remove filter';
+
+const TEXT_ADVANCED_SPECIALS_DISCOUNT = 'Discount:';
+const TEXT_ADVANCED_SPECIALS_DISCOUNT_POC = 'Currency or Percent';
+const TEXT_ADVANCED_SPECIALS_DISCOUNT_STARTDATE = 'Start Date';
+const TEXT_ADVANCED_SPECIALS_DISCOUNT_DATE = 'End Date';
+const TEXT_ADVANCED_SPECIALS_DISCOUNT_DATE_HELP = 'yyyy-mm-dd';
+const TEXT_ADVANCED_SPECIALS_DISCOUNT_APPLY = 'Apply discount';
+
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_MODEL = 'Model Number';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_PRODUCTS = 'Product Name';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_PRICE = 'Price';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_NET = 'Net';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_GROSS = 'Gross';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_DISCOUNTED_PRICE = 'Discounted price';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_DISCOUNT_PERCENT = 'Discount (%)';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_START_DATE = 'Start Date<br/>(yyyy-mm-dd)';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_DATE = 'End Date<br/>(yyyy-mm-dd)';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_STATUS = 'Status';
+const TEXT_ADVANCED_SPECIALS_TABLE_HEADING_ACTIONS = 'Actions';
+
+const TEXT_ADVANCED_SPECIALS_ACTIVATE_ALL = 'Activate all';
+const TEXT_ADVANCED_SPECIALS_DEACTIVATE_ALL = 'Deactivate all';
+const TEXT_ADVANCED_SPECIALS_REMOVE_ALL = 'Remove all';
+
+const TEXT_ADVANCED_SPECIALS_TABLE_UPDATE = 'Update';
+const TEXT_ADVANCED_SPECIALS_TABLE_REMOVE = 'Remove';
+
+const TEXT_ADVANCED_SPECIALS_GENERAL_CONFIRM = 'Are you sure?';
+const TEXT_ADVANCED_SPECIALS_MODAL_CONFIRM = 'Confirm Action!';
+const TEXT_ADVANCED_SPECIALS_BUTTON_YES = 'YES';
+const TEXT_ADVANCED_SPECIALS_BUTTON_NO = 'NO';
+
+const TEXT_ADVANCED_SPECIALS_GENERAL_INFO = 'Information';
+const TEXT_ADVANCED_SPECIALS_GENERAL_COMMENT = '<div class="collapse" id="advSpInfo"><div class="card bg-warning mb-3"><div class="card-header">Advanced Specials Info<small class="float-sm-right">' . TEXT_ADVANCED_SPECIALS_VERSION . '</small></div><div class="card card-body"><p class="card-text"><ul><li>When you open this page for the first time, the necessary database changes will be done automatically.</li><li>You may filter and sort your products to set up offers.</li><li>You can set the same discount for all filtered products by filling out the discount field, choosing a start and end date if needed and pressing the "apply discount" button.</li><li>If you don\'t enter a start date, you have to start the offers individually in the respective line, or all together with the "activate all" button</li><li>Offers are entered in gross or net, depending on what the shop is displaying (configuration->my store->display prices with tax).</li><li>You can also enter the offers in percent by simply adding a &#037;-symbol at the end.</li><li>The activation of specials with a start date is done automatically at the first time a client loads the store frontend.</li><li>When you deactivate specials, if the start date is set it will be removed. This is done to avoid manually deactivated specials to be re-activated automatically.</li></ul></div></div></div>';
